eec70ed829589716156d5b0b0ff44d2e3815c297
John LaMaster <7785925+JohnLaMaster@users.noreply.github.com>
2024-04-12 13:56:23 +0200

    Update imtl.py
    Still need to do final testing, but I reworked it to simplify the implementation and correct some bugs

--- imtl.py
+++ imtl.py
@@ -1,57 +1,67 @@
 import torch
 import torch.nn as nn
 
+from typing import Optional
+
 
 __all__ = ['IMTL']
 
 
 class IMTL(nn.Module):
-    def __init__(self, method: str='hybrid', num_losses: int=1):
+    '''Backward must be called on all outputs'''
+    def __init__(self, method: str='hybrid', num_losses: int=1, init_val: Optional[list]=None):
         super().__init__()
         self.method = method
-        self.first = True
         self.num_losses = -1
-        self.ind = []
-        self.register_buffer('e', torch.exp(torch.ones([1])))
-        self.register_parameter('s_t', torch.ones(num_losses+1).squeeze())
-        
-    def instantiate(self, 
-                    device,
-                    losses: list[torch.Tensor, ...]):
-        self.device = data.device
-        for i, loss in enumerate(losses):
-            self.ind.append(-1)
-            if loss.requires_grad: 
-                self.num_losses += 1
-                self.ind[-1] = self.num_losses
-        self.first = False
+        init_loss = [1 for _ in range(num_losses)] if not isinstance(init_val, list) else [x for x in init_val]
+        if 'gradient' in method:
+            self.register_buffer('s_t', torch.tensor(init_loss))
+        else:
+            self.register_parameter('s_t', torch.tensor(init_loss))
         
     def forward(self, 
-                data: torch.Tensor,
+                shared_parameters,
                 losses: list[torch.Tensor, ...],
-               ) -> tuple([torch.Tensor,...]):
-        if isinstance(self.first, True): self.instantiate(data.device, losses)
-            
+               ) -> tuple[torch.Tensor, torch.Tensor]:
         # >>> Loss Balance
-        L_t = torch.empty(self.s_t.numel(), device=self.device)
-        g_t = torch.empty([self.num_losses+1, 1, data.shape[-1]], device=self.device)
-        for i, loss in zip(self.ind, losses):
-            if loss.requires_grad:
-                L_t[i] = loss * self.e.pow(self.s_t[i]) - self.s_t[i]
-                g_t[i,::] = torch.autograd.grad(L_t[i], data, retain_graph=True, create_graph=True)[0].mean(dim=0, keepdim=True)
-        u_t = g_t / (torch.linalg.norm(g_t, 2, (-1,-2)) + 1e-6).unsqueeze(-1).unsqueeze(-1)
+        L_t, g, u = [], [], []
+        
+        for i, l in enumerate(losses):
+            if self.method in ['hybrid','loss']:
+                # If 'hybrid' or 'loss' method selected, then learnable scale s_t is used
+                L_t.append(l * self.e.pow(self.s_t[i]) - self.s_t[i])
+            else:
+                # Else, the losses are unscaled
+                L_t.append(l)
+
+            g[i].append(torch.flatten(torch.autograd.grad(L_t[-1], shared_parameters, retain_graph=True)))
+            u[i].append(g[i] / torch.linalg.norm(g[i]))
+            
+        g_t = torch.stack([_g for _g in g])
+        u_t = torch.stack([_u for _u in u])
        
         # >>> Gradient Balance
-        D = g_t[0,::].unsqueeze(0).repeat(self.num_losses,1,1) - g_t[1:,::]
-        UT = u_t[0,:].unsqueeze(0).repeat(self.num_losses,1,1).mT - u_t[1:,::].mT
+        D = g_t[0,...] - g_t[1:,...]
+        U = u_t[0,...] - u_t[1:,...]
 
-        alpha_2T = g_t[0,::].unsqueeze(0).matmul(UT).matmul(torch.linalg.pinv(D.matmul(UT)))
-        alpha = torch.cat([torch.ones([1, alpha_2T.shape[1], alpha_2T.shape[2]], device=self.device) - 
-                           alpha_2T.sum(dim=0, keepdim=True), alpha_2T], dim=0).squeeze()
+        arg0 = D.matmul(U.t())
+        arg1 = torch.eye(arg0.shape[-1].data)
+        alpha_2T = g_t[0,...].matmul(U.t()).matmul(torch.linalg.solve(arg0,arg1))
         
+        alpha = torch.cat([torch.ones(1, device=self.device) - alpha_2T.sum(dim=0, keepdim=True), 
+                           alpha_2T], dim=0).squeeze()
+        
+        # All methods return two outputs: gradient loss and individual losses
         if self.method=='hybrid':
-            return torch.sum(L_t * alpha), [loss.backward(retain_graph=True) for i, loss in enumerate(L_t)]
+            return torch.sum(L_t * alpha), L_t
         elif self.method=='gradient':
-            return torch.sum(L_t * alpha)
+            return torch.sum(L_t * alpha), None
         elif self.method=='loss':
-            return [loss.backward(retain_graph=True) for i, loss in enumerate(L_t)]
+            return None, L_t
+        
+    def parameters(self) -> List[torch.Tensor]:
+        '''return learnable parameters to be added to the optimizer'''
+        if not 'gradient' in self.method:
+            return [self.s_t]
+        return []
+    
